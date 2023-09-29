418323ce0f17e11b6e8e36a924f3c0ef617f1c79
joecarl <joecarlhr@gmail.com>
2023-09-29 20:20:25 +0000

    Implement NetGroupsHandler

---
+++ include/dp/client/netgroupshandler.h
@@ -0,0 +1,66 @@
+#ifndef NETGROUPSHANDLER_H
+#define NETGROUPSHANDLER_H
+
+#include <dp/serendipia.h>
+#include <dp/client/netgroup.h>
+#include <dp/client/neteventslistenershandler.h>
+#include <boost/json.hpp>
+#include <map>
+#include <memory>
+
+namespace dp::client {
+	class BaseClient;
+}
+
+namespace dp::client {
+
+typedef struct {
+
+	std::string id;
+
+	std::string owner_name;
+
+	// people, size...
+
+} GroupInfo;
+
+class NetGroupsHandler {
+
+	/**
+	 * The list of groups available for the client.
+	 */
+	std::map<std::string, GroupInfo> groups;
+
+	/**
+	 * The current group where the client belongs. If the client never joined
+	 * any group or leaved the group this value should be nullptr
+	 */
+	std::unique_ptr<NetGroup> group = nullptr;
+
+	BaseClient* client;
+
+	NetService* net;
+
+	NetEventsListenersHandler* nelh;
+	
+	/** 
+	 * Creates the event handlers for the events scoped with `groups`
+	 */
+	void setup_nelh();
+
+public:
+
+	NetGroupsHandler(BaseClient* client);
+
+	/**
+	 * Sends the `join_group` request to the server
+	 */
+	void join_group(const std::string& id);
+
+	NetGroup* get_current_group() { return this->group.get(); }
+
+};
+
+} // namespace dp::client
+
+#endif
---
+++ src/client/netgroupshandler.cpp
@@ -0,0 +1,79 @@
+
+#include <dp/client/netgroupshandler.h>
+#include <dp/client/baseclient.h>
+//#include <stdexcept>
+#include <iostream>
+
+using std::cerr;
+using std::endl;
+using boost::json::object;
+
+namespace dp::client {
+
+static GroupInfo parse_group_info(object& g) {
+	return GroupInfo {
+		.id = g["id"].as_string().c_str(),
+		.owner_name = g["owner_name"].as_string().c_str(),
+	};
+}
+
+NetGroupsHandler::NetGroupsHandler(BaseClient* _client) : 
+	client(_client),
+	net(&(_client->get_io_client())),
+	nelh(net->create_nelh())
+{
+	this->setup_nelh();
+}
+
+
+void NetGroupsHandler::setup_nelh() {
+
+	nelh->add_event_listener("groups/join", [this] (object& data) {
+
+		object g = data["group"].as_object();
+
+		std::string id = g["id"].as_string().c_str();
+		std::string owner_id = g["owner_id"].as_string().c_str();
+		boost::json::array members = g["members"].as_array();
+		
+		this->group = std::make_unique<NetGroup>(this->net, id, owner_id, members);
+
+	});
+
+	nelh->add_event_listener("groups/leave", [this] (object& data) {
+		// this destroys the group?
+		this->group = nullptr;
+	});
+
+	// This event is received when the client connects to the server
+	nelh->add_event_listener("groups/info", [this] (object& data) {
+		this->groups.clear();
+		boost::json::array g_arr = data["groups"].as_array();
+		for (auto& g: g_arr) {
+			auto g_info = parse_group_info(g.as_object());
+			this->groups[g_info.id] = g_info;
+		}
+	});
+
+	auto on_create_or_update = [this] (object& data) {
+		auto g_info = parse_group_info(data["group"].as_object());
+		this->groups[g_info.id] = g_info;
+	};
+	nelh->add_event_listener("groups/create", on_create_or_update);
+	nelh->add_event_listener("groups/upate", on_create_or_update);
+
+	nelh->add_event_listener("groups/destroy", [this] (object& data) {
+		std::string g_id = data["id"].as_string().c_str();
+		this->groups.erase(g_id);
+	});
+
+}
+
+void NetGroupsHandler::join_group(const std::string& id) {
+
+	this->net->send_request("groups/join", { {"id", id} });
+	
+}
+
+	
+} // namespace dp::client
