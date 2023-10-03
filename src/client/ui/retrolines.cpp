59f2570102ddef855ce3e12f68ec25f76e659413
joecarl <joecarlhr@gmail.com>
2023-10-03 17:01:15 +0000

    Add flicker effect and use cga colors

--- src/client/ui/retrolines.cpp
+++ src/client/ui/retrolines.cpp
@@ -1,4 +1,5 @@
 #include <dp/client/ui/retrolines.h>
+#include <dp/client/mediatools.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <cmath>
@@ -69,8 +70,33 @@ void RetroLines::draw(float ox, float oy) {
 			const float x = ox + dash.x * mult_x;
 			const float y = oy + line_num * mult_y * 2 + sin(time + x);
 			const float x2 = x + dash.width * mult_x;
-			const float y2 = y + mult_y;
-			al_draw_filled_rectangle(x, y, x2, y2, al_map_rgb(180, 255, 180));
+			const float y2 = y + mult_y / 2.0;
+			const float y3 = y + mult_y;
+			//al_draw_filled_rectangle(x, y, x2, y2, al_map_rgb(180, 255, 180));
+			uint32_t phase = (uint32_t)(this->time * 1.4) % 4;
+			uint32_t phase2 = (uint32_t)(this->time) % 70;
+			ALLEGRO_COLOR c1, c2;
+			if (phase2 < 56) {
+				c1 = CGA_BLUE;
+				c2 = CGA_BLUE;
+			} else if (phase == 0) {
+				c1 = CGA_PINK;
+				c2 = CGA_BLUE;
+			} else if (phase == 1) {
+				c1 = CGA_PINK;
+				c2 = CGA_PINK;
+			} else if (phase == 2) {
+				c1 = CGA_BLUE;
+				c2 = CGA_PINK;
+			} else if (phase == 3) {
+				c1 = CGA_BLUE;
+				c2 = CGA_BLUE;
+			}
+
+			al_draw_filled_rectangle(x, y, x2, y2, c1); 
+			al_draw_filled_rectangle(x, y2, x2, y3, c2);
+			//al_draw_filled_rectangle(x, y, x2, y3, sine > 0.5 ? WHITE : CGA_BLUE);
+			//al_draw_filled_rectangle(x, y, x2, y3, CGA_BLUE);
 		}
 		line_num++;
 	}
