41fd3675a1543d85917407c1097e23ade8f2f9c9
joecarl <joecarlhr@gmail.com>
2023-10-06 12:07:21 +0000

    Extract input common methods and properties

--- include/dp/client/ui/input.h
+++ include/dp/client/ui/input.h
@@ -4,6 +4,10 @@
 #include <boost/json.hpp>
 #include <string>
 
+namespace dp::client {
+	class BaseClient;
+}
+
 namespace dp::client::ui {
 
 enum InputType {
@@ -13,7 +17,27 @@ enum InputType {
 	INPUT_TYPE_MAX
 };
 
+enum InputValueType {
+	INPUT_VALUE_TYPE_STRING,
+	INPUT_VALUE_TYPE_BOOL,
+	INPUT_VALUE_TYPE_DOUBLE,
+	INPUT_VALUE_TYPE_INTEGER,
+	INPUT_VALUE_TYPE_MAX
+};
+
 class Input {
+
+protected:
+	
+	/**
+	 * The font used to draw the text
+	 */
+	BaseClient* const engine;
+
+	InputType type;
+
+	InputValueType value_type;
+
 public:
 
 	std::string label;
@@ -22,15 +46,17 @@ public:
 
 	//double y;
 
-	InputType type;
+	Input(BaseClient* _engine);
 
 	virtual void draw(float x, float y) = 0;
 
-	virtual void focus() = 0;
+	void focus();
 
-	virtual void blur() = 0;
+	void blur();
 
-	virtual bool is_focused() = 0;
+	bool is_focused();
+
+	virtual void process_key(wchar_t ASCII, int control_key) { }
 	
 	virtual bool is_valid() = 0;
 
@@ -39,6 +65,10 @@ public:
 	virtual void set_from_json_value(boost::json::value& val) = 0;
 
 	virtual boost::json::value get_json_value() = 0;
+
+	InputType get_type() { return this->type; }
+
+	InputValueType get_value_type() { return this->value_type; }
 
 };
 
--- src/client/ui/input.cpp
+++ src/client/ui/input.cpp
@@ -1,2 +1,32 @@
 #include <dp/client/ui/input.h>
+#include <dp/client/baseclient.h>
 
+namespace dp::client::ui {
+
+Input::Input(BaseClient* _engine) : engine(_engine) { 
+
+}
+
+void Input::focus() {
+	
+	this->engine->set_active_input(this);
+	
+}
+
+
+bool Input::is_focused() {
+
+	return this->engine->get_active_input() == this;
+
+}
+
+
+void Input::blur() {
+
+	if (this->is_focused()) {
+		this->engine->set_active_input(nullptr);
+	} 
+
+}
+
+} // namespace dp::client::ui
