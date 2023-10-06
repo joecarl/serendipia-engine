acb68c1ab7d0cbb166dff87fa6eaafd9e8913866
joecarl <joecarlhr@gmail.com>
2023-10-06 12:08:16 +0000

    Implement SelectInput

---
+++ include/dp/client/ui/selectinput.h
@@ -0,0 +1,65 @@
+#ifndef SELECTINPUT_H
+#define SELECTINPUT_H
+
+#include <dp/client/ui/input.h>
+#include <string>
+#include <vector>
+
+namespace dp::client { 
+	class BaseClient;
+}
+
+namespace dp::client::ui {
+
+//template <typename T>
+struct SelectOption {
+
+	std::string label;
+
+	std::string value;
+
+};
+
+class SelectInput : public Input {
+	
+	/**
+	 * String iterator
+	 */
+	std::vector<SelectOption>::iterator iter;
+
+	/**
+	 * The array of options
+	 */
+	std::vector<SelectOption> options;
+
+public:
+
+	SelectInput(BaseClient* _engine);
+
+	void set_options(std::vector<SelectOption>& opts);
+	
+	void set_value_type(InputValueType vtype);
+
+	void reset();
+
+	void draw(float x, float y);
+
+	bool is_valid();
+	
+	void process_key(wchar_t ASCII, int control_key);
+
+	std::string get_validation_msg();
+	
+	void set_from_json_value(boost::json::value& val);
+
+	boost::json::value get_json_value();
+
+	std::string get_value();
+
+	bool get_value_as_bool();
+
+};
+
+} // namespace dp::client::ui
+
+#endif
---
+++ src/client/ui/selectinput.cpp
@@ -0,0 +1,137 @@
+#include <dp/client/ui/selectinput.h>
+#include <dp/client/mediatools.h>
+#include <dp/client/baseclient.h>
+
+#include <allegro5/allegro_primitives.h>
+#include <iostream>
+
+namespace dp::client::ui {
+
+SelectInput::SelectInput(BaseClient* _engine) :
+	Input(_engine)
+{
+	this->type = INPUT_TYPE_SELECT;
+	this->value_type = INPUT_VALUE_TYPE_STRING;
+	reset();
+
+}
+
+
+void SelectInput::set_options(std::vector<SelectOption>& opts) {
+
+	this->options = opts;
+	this->reset();
+
+}
+
+void SelectInput::set_value_type(InputValueType vtype) {
+	this->value_type = vtype;
+}
+
+
+
+void SelectInput::reset() {
+
+	this->iter = this->options.begin();
+
+}
+
+
+void SelectInput::process_key(wchar_t ASCII, int control_key) {
+
+	if (this->options.size() == 0) {
+		return;
+	}
+
+	auto last_item = this->options.end() - 1;
+	bool loop = true;
+
+	if (control_key == ALLEGRO_KEY_RIGHT) {
+		if (iter < last_item) {
+			iter++;
+		} else if (loop) {
+			iter = this->options.begin();
+		}
+	} else if (control_key == ALLEGRO_KEY_LEFT) {
+		if (iter > this->options.begin()) {
+			iter--;
+		} else if (loop) {
+			iter = last_item;
+		}
+	}
+	
+}
+
+
+void SelectInput::draw(float x, float y) {
+
+	ALLEGRO_FONT* font = this->engine->get_font();
+
+	al_draw_text(font, WHITE, x, y, ALLEGRO_ALIGN_LEFT, iter->label.c_str());
+
+	if (!this->is_focused()) {
+		return;
+	}
+	// TODO: draw arrows
+}
+
+
+std::string SelectInput::get_value() {
+
+	return iter->value;
+
+}
+
+
+bool SelectInput::get_value_as_bool() {
+	
+	return this->get_value() == "true";
+
+}
+
+
+void SelectInput::set_from_json_value(boost::json::value& val) {
+
+	std::string v;
+	if (val.is_string()) {
+		v = val.get_string().c_str();
+	} else if (val.is_bool()) {
+		v = val.get_bool() ? "true" : "false";
+	} else {
+		return;
+	}
+
+	auto it = options.begin();
+	while (it != options.end() && it->value != v) {
+		it++;
+	}
+	iter = it;
+	
+}
+
+
+boost::json::value SelectInput::get_json_value() {
+
+	if (this->value_type == INPUT_VALUE_TYPE_BOOL) {
+		return boost::json::value(this->get_value_as_bool());
+	}
+	// TODO: implement more conversions
+	return boost::json::value(this->get_value());
+
+}
+
+
+bool SelectInput::is_valid() {
+	
+	return iter >= options.begin() && iter < options.end();
+
+}
+
+
+std::string SelectInput::get_validation_msg() {
+
+	return "";
+
+}
+
+} // namespace dp::client::ui
